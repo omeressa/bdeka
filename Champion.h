e422b5a9ad85bc1b947ca831fbf38703b7094ac2
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-20 13:49:25 +0300

    Update Champion.h

--- Champion.h
+++ Champion.h
@@ -5,10 +5,8 @@
 class Champion : public Player
 {
 public:
-/*
-         const string name() const override {return "OMAR";};
-         const Coordinate play(const Board& board) override ;
-         */
+
         virtual const string name()const{return "omar";}
         virtual const Coordinate play(const Board& board) override;
+        
 };
