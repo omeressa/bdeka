ac17746967d9256cc42c06e7fb70e54c8cbdefe6
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-20 14:09:44 +0300

    Update TicTacToe.h

--- TicTacToe.h
+++ TicTacToe.h
@@ -8,10 +8,10 @@ public:
     Board game;
     int sizeOfBoard;
     Player* champion;
+    
     TicTacToe(int sizeOfBoard);
     void play(Player& xPlayer, Player& oPlayer);
     bool isWinner(char c);
-    Board board() const;
-    Player& winner() const;
-
+    Board board() const{return game;};
+    Player& winner() const{return *champion;};
 };
