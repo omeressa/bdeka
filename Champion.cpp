95d0483bec6ae8059972f40bf70da42d77b0ab90
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-20 13:58:36 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -1,37 +1,14 @@
-/*
-#include "Champion.h"
-const Coordinate Champion::play(const Board& board) {
-    for (uint i=0; i<board.size(); ++i) {
-        uint t = board.size()-1-i;
-        Coordinate c{t,i};
-        t = board.size()-1;
-        if(i==1 && board[{0,t}] == '.'){
-            c.setIndexI(0); c.setIndexJ(board.size()-1);
-        }
-        if(board[c]=='.') {
-            return c;
-        }
-    }
-    return {0,0};
-}
-*/
 #include "Champion.h"
 
-const Coordinate Champion::play(const Board& board){
-    if(board[{0,board.size()-1}]=='.'){
-        return {0,board.size()-1};
-    }
-    if(board[{board.size()-1,0}]=='.'){
-        return {board.size()-1,0};
-    }
-    uint i = board.size()-1, j = 0;
-    while(j<board.size()){
-        Coordinate c{i,j};
-	    if (board[c]=='.') {
-			return c;
-		}
-        i--;
-        j++; 
+const Coordinate Champion::play(const Board& board) {
+    for (uint j=0; j<board.size(); ++j) {
+        uint i = board.size()-1-j;
+        Coordinate temp{i,j};
+        i = board.size()-1;
+        if(j==1 && board[{0,i}] == '.'){
+            temp.setIndexI(0); temp.setIndexJ(board.size()-1);
+        }
+        if(board[temp]=='.') {return temp;}
     }
     return {0,0};
 }
