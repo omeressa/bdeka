db9637f55e404621b42e09d80a8bc9731a910994
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-20 14:30:39 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -1,5 +1,5 @@
 #include "Champion.h"
-
+/*
 const Coordinate Champion::play(const Board& board) {
     for (uint j=0; j<board.size(); ++j) {
         uint i = board.size()-1-j;
@@ -11,6 +11,26 @@ const Coordinate Champion::play(const Board& board) {
             temp.setIndexI(0); temp.setIndexJ(board.size()-1);
         }
                         if(board[temp]=='.') {return temp;}
+    }
+    return {0,0};
+}*/
+
+
+const Coordinate Champion::play(const Board& board){
+    if(board[{0,board.size()-1}]=='.'){
+        return {0,board.size()-1};
+    }
+    if(board[{board.size()-1,0}]=='.'){
+        return {board.size()-1,0};
+    }
+    uint i = board.size()-1, j = 0;
+    while(j<board.size()){
+        Coordinate c{i,j};
+	    if (board[c]=='.') {
+			return c;
+		}
+        i--;
+        j++; 
     }
     return {0,0};
 }
